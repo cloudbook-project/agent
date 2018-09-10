e7efb62df9ab11ddd021ff0cc285f67ef4bf6b31
jjaranda13 <jjaranda13@alcatel-lucent.com>
2018-09-10 16:29:37 +0200

    agent

--- agent.py
+++ agent.py
@@ -10,7 +10,7 @@ application = Flask(__name__)
 
 @application.route("/", methods=['GET', 'PUT', 'POST'])
 def hello():
-	print "llamada"
+	print "hello world"
 	return  "Hello"
 
 @application.route("/invoke", methods=['GET','POST'])
@@ -20,11 +20,28 @@ def invoke():
 	#y si no, hay que invocar al agente que la contenga, consultando el cloudbook
 	#llamando al modulo invoker.py
 	
+	# example of invocation
+	# http://localhost:3000/invoke?invoked_function=compute(56,77,5)
+	invoked_function=request.args.get('invoked_function')
+
+	# check if invoked function belongs to this agent, otherwise will re-invoke to the right agent
+	# PENDIENTE DE PROGRAMAR
+
+	print "invoked_function = "+invoked_function
 	#supongamos que procesamos el post y llega esto
-	invoked_function="main()"
+	# invoked_function="main()"
 	#os.chdir('./du_files')
 	#exec("import du_0")
-	return eval("du_0."+invoked_function)
+	#return eval("du_0."+invoked_function)
+	#return eval("du_0."+"main("+"cosa"+")")
+	a= eval("du_0."+"main()")
+	print "funcion terminada ok"
+	print a
+	return a
+
+def cosa(k):
+	print k
+	return "cloudbook"
 
 
 
@@ -33,9 +50,20 @@ if __name__ == "__main__":
 	du_list = []
 	du_list = importer.load_cloudbook()
 	importer.import_dus(du_list)
+
+	du_list=["du_0"] # fake
+	
+	j = du_list[0].rfind('_')+1
+	num_du = du_list[0][j:]
+	# num_du is the initial DU and will be used as offset for listen port
+	print num_du
 	#os.chdir("./du_files")
 	#exec("import du_0")
-	exec ("from du_files import du_0")
+	# du_files is the distributed directory containing all DU files
+	for du in du_list:
+		exec ("from du_files import "+du)
+
+	du_0.invoker=cosa
 	print "hola"
 	du_0.main()
-	application.run(debug=True, host='0.0.0.0', port = 3000)
+	application.run(debug=True, host='0.0.0.0', port = 3000+int(num_du))
--- du_files/du_0.py
+++ du_files/du_0.py
@@ -1,9 +1,24 @@
 import threading
 
-def main():
-	#invoke("du_1.f1()")
+invoker=None
+
+def f0():
+	invoker('du_1.f1()')
 	f3()
-	return "hola2"
+
+
+	return 'cloudbook' 
 
 def f3():
 	print "dir2.file3.f3: Hello world"
+
+
+	return 'cloudbook' 
+
+def main():
+	f0()
+	return "cloudbook"
+
+if __name__ == '__main__':
+	f0()
+			
